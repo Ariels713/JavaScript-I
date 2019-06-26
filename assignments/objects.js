7a7a260cdbf34a983443b4b031b7bb72a938443e
Ariel Rodriguez <ariel_rodriguez@ariels-mac-mini.lan>
2019-06-26 15:39:33 -0400

    finished objects.js

--- assignments/objects.js
+++ assignments/objects.js
@@ -10,35 +10,78 @@
 // 5,adaine5@samsung.com,Antonietta,F
 
 // Example format of an intern object: 1,examples@you.edu,Example,F
-const example = {
-  "id": 0,
-  "name": "Example",
-  "email": "examples@you.edu",
-  "gender": "F"
-}
+// const example = {
+//   "id": 0,
+//   "name": "Example",
+//   "email": "examples@you.edu",
+//   "gender": "F"
+// }
 
 // Write your intern objects here:
+
+const mitzi_F = {
+  "id": 010,
+  "name": "Mitzi Franz",
+  "email": "mmelloy0@psu.edu",
+  "gender": "F"
+}
+const kennan_M = {
+  "id": 010,
+  "name": "Kennan Malcolm",
+  "email": "kdiben1@tinypic.com",
+  "gender": "M"
+}
+const keven_M = {
+  "id": 011,
+  "name": "Keven Marsh",
+  "email": "kmummery2@wikimedia.org",
+  "gender": "M"
+}
+const gennie_M = {
+  "id": 012,
+  "name": "Gennie Alba",
+  "email": "gmartinson3@illinois.edu",
+  "gender": "F"
+}
+const antonie_E = {
+  "id": 013,
+  "name": "Anntonette Ezra",
+  "email": "adaine5@samsung.com",
+  "gender": "F"
+}
 
 
 // ==== Challenge 2: Reading Object Data ==== 
 // Once your objects are created, log out the following requests from HR into the console:
 
 // Mitzi's name
-
+console.log(mitzi_F.name)//?
 // Kennan's ID
-
+kennan_M.id//?
 // Keven's email
-
+kennan_M.email//?
 // Gannie's name
-
+gennie_M.name//?
 // Antonietta's Gender
+antonie_E.gender//?
+
 
 // ==== Challenge 3: Object Methods ==== 
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
 // console.log(kennan.speak());
+kennan_M.greeting = function(greet) {
+  console.log(`Hello ${greet}, my name is ${kennan_M.name}`)
+}
+// kennan_M.greeting('ariel') Why am I getting undefined? 
+
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
+antonie_E.multiplies = function(num1, num2) {
+  return num1 * num2;
+}
+
+// antonie_E.multiplies(5,2)//?
 
 // === Great work! === Head over to the the arrays.js file or take a look at the stretch challenge
 
